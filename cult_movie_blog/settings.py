a35f077a63b04df3d73b25738adaa7157cec7115
jonathan <jonat7502@yahoo.com>
2016-12-01 16:19:55 +0000

    Fixed DB Connect String

--- cult_movie_blog/settings.py
+++ cult_movie_blog/settings.py
@@ -91,12 +91,12 @@ WSGI_APPLICATION = 'cult_movie_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-#CLEARDB_DATABASE_URL = os.environ.get("CLEARDB_DATABASE_URL", "")
-#DATABASES ['default'] = dj_database_url.parse(CLEARDB_DATABASE_URL)
+CLEARDB_DATABASE_URL = os.environ.get("CLEARDB_DATABASE_URL", "")
+DATABASES ['default'] = dj_database_url.parse(CLEARDB_DATABASE_URL)
 
 
 # Password validation
